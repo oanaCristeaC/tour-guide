b70cc3a2217049d518c7b464052ab9af3b0ac860
Ionela Cristea <oanacristea.1007@yahoo.com>
2020-10-09 17:55:03 +0200

    connection to db

--- server.js
+++ server.js
@@ -1,8 +1,22 @@
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 
 dotenv.config({
   path: './config.env',
 });
+
+const db = process.env.DB.replace('<PASSWORD>', process.env.DB_PASS)
+
+mongoose.connect(db, {
+//mongoose.connect(process.env.DB_LOCAL, {
+  // useNewUrlParser: true,
+  // useCreateIndex: true,
+  // useFindAndModify: true
+}).then(con => {
+  // console.log(con.connection)
+  console.log("app running")
+})
+
 const app = require('./app');
 
 const port = process.env.PORT || 8000;
