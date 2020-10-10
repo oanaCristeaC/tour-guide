bd3174b78f2670a7ad403e270b64df32b7d95802
Ionela Cristea <oanacristea.1007@yahoo.com>
2020-10-10 22:42:27 +0200

    modeling the tour

--- models/tourModel.js
+++ models/tourModel.js
@@ -4,16 +4,59 @@ const tourSchema = new mongoose.Schema({
     name: {
       type: String,
       required: [true, 'Tour must have a name.'],
-      unique: true
+      unique: true,
+      trim: true
+    },
+    duration: {
+      type: Number,
+      required: [true, 'Tour must have a duration.']
+    },
+    maxGroupSize: {
+      type:Number,
+      required: [true, 'Tour must have a group size.']
+    },
+    difficulty: {
+      type: String,
+      required: [true, 'Tour must have a difficulty.'],
+      trim: true
     },
     ratting: {
+      type: Number,
+      default: 0
+    },
+    rattingAverage: {
+      type: Number,
+      default: 0
+    },
+    rattingQuantity: {
       type: Number,
       default: 0
     },
     price: {
       type: Number,
       required: [true, 'Tour must have a price.']
-    }
+    },
+    priceDiscount: Number,
+    summary: {
+      type: String,
+      required: [true, 'A tour must have a summary'],
+      trim: true // Removes blanc spaces
+    },
+    description: {
+      type: String,
+      trim: true
+    },
+    imageCover: {
+      type: String,
+      required: [true, 'A tour must have cover image.']
+    },
+    images: [String],
+    createdAt: {
+      type: Date,
+      default: Date.now()
+    },
+    startDates: [Date],
+    
   });
   
 const Tour = mongoose.model('Tour', tourSchema);
