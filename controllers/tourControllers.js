1ea9d50cf84954207c15abe1153351ba755dabf4
Ionela Cristea <oanacristea.1007@yahoo.com>
2020-10-11 20:13:06 +0200

    added sort query

--- controllers/tourControllers.js
+++ controllers/tourControllers.js
@@ -2,19 +2,31 @@ const Tour = require('../models/tourModel')
 
 exports.getTours = async (req, res) => {
   try {
+
+    /** Filer */
+
     // Simple query filter; excluding: page, limit, sort and field
     const queryObj = { ...req.query};
-    const excludedQuery = ['page', 'limit', 'sort', 'field'];
-    excludedQuery.forEach(el => delete queryObj[el]);
-    console.log("queryObj", queryObj)
+    const excludedQuery = ['page', 'limit', 'sort', 'field']; 
+    excludedQuery.forEach(el => delete queryObj[el]); //TODO
 
     // Advance query filter with operators: gt, gte, lt and lte (duration[lte]=5)
     let queryStr = JSON.stringify(queryObj)
     queryStr = queryStr.replace(/\b(gt|gte|lt|lte)\b/g, match => `$${match}`)
 
-    const query = Tour.find(JSON.parse(queryStr));
-    const tours = await query;
 
+    /**Sort */
+    let query = Tour.find(JSON.parse(queryStr));
+    if (req.query.sort) {
+      // sort by 
+      //query = query.sort(req.query.sort) // single sort
+      const sortBy = req.query.sort.split(',').join(' ') // in url use comma, in mongo use space
+      query = query.sort(sortBy) 
+    } else {
+      query = query.sort('-createdAt')
+    }
+
+    const tours = await query;
     res.status(200).json({
       status: 'Success',
       requests: tours.length,
