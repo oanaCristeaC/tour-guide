c97187009819c1a6619454d09063494ca29a26e5
Ionela Cristea <oanacristea.1007@yahoo.com>
2020-10-16 19:34:17 +0200

    validations with 3rd parties

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,12 +1,25 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
+const validate = require('mongoose-validator');
+const validator = require('validator');
+
+const dificValidator = [
+  validate({
+    validator: 'matches',
+    arguments: ['easy, medium or difficult'],
+    message: 'Difficulty must be: {ARGS[0]}',
+  })
+]
 
 const tourSchema = new mongoose.Schema({
   name: {
     type: String,
     required: [true, 'Tour must have a name.'],
     unique: true,
-    trim: true
+    trim: true,
+    minlength: [5, `A tour name must have a min of 5 characters`],
+    maxlength: [40, 'A tour name must have a max of 40 characters'],
+    //validate: [validator.isAlpha, 'Nume must have only letters'] // does not allow spaces
   },
   duration: {
     type: Number,
@@ -19,11 +32,18 @@ const tourSchema = new mongoose.Schema({
   difficulty: {
     type: String,
     required: [true, 'Tour must have a difficulty.'],
-    trim: true
+    trim: true,
+    validate: dificValidator
+    // enum: {
+    //   values: ['easy', 'medium', 'difficult'],
+    //   message: 'Available values for difficulty are: easy, medium or difficult'
+    // }
   },
   rating: {
     type: Number,
-    default: 0
+    default: 0,
+    min: [1, 'A tour must have a min ratting value of 1.0'],
+    max: [5, 'A tour must have a max ratting value of 5.0'],
   },
   ratingsAverage: {
     type: Number,
@@ -37,7 +57,15 @@ const tourSchema = new mongoose.Schema({
     type: Number,
     required: [true, 'Tour must have a price.']
   },
-  priceDiscount: Number,
+  priceDiscount: {
+    type: Number,
+    validate: {
+      validator: function (val) {
+        return val < this.price
+      },
+      message: 'Dicount price must be lower than the price.'
+    }
+  },
   summary: {
     type: String,
     required: [true, 'A tour must have a summary'],
