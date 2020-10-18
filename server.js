63faff30efdc47ca8bac1428340e6d550738a332
Ionela Cristea <oanacristea.1007@yahoo.com>
2020-10-18 22:11:07 +0200

    unhandle rejection

--- server.js
+++ server.js
@@ -20,6 +20,13 @@ mongoose.connect(db, {
 const app = require('./app');
 const port = process.env.PORT || 8000;
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App listen at port ${port}`);
 });
+
+process.on('unhandledRejection', err => {
+  console.log('Unhandled Rejection. Shooting down...', err.name, err.message);
+  server.close(() => {
+    process.exit(1) // TODO: the server hangs. Neets a way to restatrt
+  })
+})
