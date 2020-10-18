c1ab1301ad3b32697721e1fa7f6e6a725045eda4
Ionela Cristea <oanacristea.1007@yahoo.com>
2020-10-18 19:00:46 +0200

    handle prod and dev errors

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,12 +1,36 @@
+const sendDevError = (error, res) => {
+	res.status(error.statusCode).json({
+		status: error.status,
+		message: error.message,
+		error,
+		err: error.stck
+	})
+};
+
+const sendProdError = (error, res) => {
+	// Operational trusted error
+	if (error.isOperational) {
+		res.status(error.statusCode).json({
+			status: error.status,
+			message: error.message
+		})
+	} else {
+		// Unknown programming error which we dont want to leak to the client
+		res.status(500).json({
+			status: 'Error',
+			message: 'Something went wrong!'
+		})
+	}
+}
+
 module.exports = (error, req, res, next) => {
 	error.statusCode = error.statusCode || 500;
 	error.status = error.status || 'Error';
 
-	// console.log(error.stack)
-
-	res.status(error.statusCode).json({
-		status: error.status,
-		message: error.message
-	})
+	if (process.env.NODE_ENV === 'development') {
+		sendDevError(error, res)
+	} else if (process.env.NODE_ENV === 'production') {
+		sendProdError(error, res)
+	}
 
 }
