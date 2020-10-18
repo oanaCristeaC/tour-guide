8165d7e46f1bab1ef232859dbda6349517bfe34b
Ionela Cristea <oanacristea.1007@yahoo.com>
2020-10-18 13:59:17 +0200

    route handler

--- app.js
+++ app.js
@@ -8,14 +8,16 @@ if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
 }
 
-/** Routers */
-// app.get('/', (req, res) => {
-//   res.send('Hello wold');
-// });
-
 const toursRouter = require('./routers/tourRouters');
 const usersRouter = require('./routers/userRouters');
 app.use('/api/v1/tours', toursRouter);
 app.use('/api/v1/users', usersRouter);
+
+app.all('*', (req, res, next) => {
+    res.status(404).json({
+        status: 'Failed',
+        message: `Can't find ${req.originalUrl}`
+    })
+})
 
 module.exports = app;
