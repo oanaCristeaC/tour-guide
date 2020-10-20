c773392f18281a7316a6ad3a27184f23eb17f0f8
Ionela Cristea <oanacristea.1007@yahoo.com>
2020-10-20 15:18:27 +0200

    security mistake

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -4,11 +4,17 @@ const catchAsync = require('../utils/catchAsync')
 // User logic
 exports.signUp = catchAsync(async (req, res, next) => {
 
-  const newUser = await User.create(req.body);
+  const newUser = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm
+  });
   res.status(200).json({
     status: 'Succes',
     data: {
-      newUser
+      name: newUser.name,
+      email: newUser.email
     }
   })
 });
