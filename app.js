1822c83a79ea04fae57e8cd8dcfce03110d4e4b8
Ionela Cristea <oanacristea.1007@yahoo.com>
2020-10-27 23:01:57 +0100

    limit requests per h

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
 const express = require('express');
 const app = express();
 const AppError = require('./utils/appError');
-const errorController = require('./controllers/errorController')
+const errorController = require('./controllers/errorController');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
 
+// Enable if you're behind a reverse proxy (Heroku, Bluemix, AWS ELB, Nginx, etc)
+// see https://expressjs.com/en/guide/behind-proxies.html
+// app.set('trust proxy', 1);
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // limit each IP to 100 requests per windowMs
+});
+
+app.use(limiter);
 app.use(express.json());
 
 if (process.env.NODE_ENV === 'development') {
-	app.use(morgan('dev'));
+  app.use(morgan('dev'));
 }
 
 const toursRouter = require('./routers/tourRouters');
@@ -17,7 +28,7 @@ app.use('/api/v1/users', usersRouter);
 
 // Handle all unset routers
 app.all('*', (req, res, next) => {
-	next(new AppError(`Route ${req.originalUrl} can't be found.`, 404));
+  next(new AppError(`Route ${req.originalUrl} can't be found.`, 404));
 });
 
 // Error handelling middleware
