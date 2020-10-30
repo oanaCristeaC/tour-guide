b2cc37c94d780a15e0ee64088be7b981f7d5e324
Ionela Cristea <oanacristea.1007@yahoo.com>
2020-10-30 13:26:17 +0100

    start modeling db

--- data/import-tours.js
+++ data/import-tours.js
@@ -1,57 +1,56 @@
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
-const fs = require('fs')
-const Tour = require('../models/tourModel')
+const fs = require('fs');
+const Tour = require('../models/tourModel');
 
 dotenv.config({
-	path: './config.env',
+  path: './config.env',
 });
 
 // Connection to DB
-const db = process.env.DB.replace('<PASSWORD>', process.env.DB_PASS)
+const db = process.env.DB.replace('<PASSWORD>', process.env.DB_PASS);
 
-mongoose.connect(db, {
-	useNewUrlParser: true,
-	useCreateIndex: true,
-	useFindAndModify: true,
-	useUnifiedTopology: true
-
-}).then(() => console.log("DB connection Successful"));
-
+mongoose
+  .connect(db, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('DB connection Successful'));
 
 const importData = async (req, res) => {
+  const data = JSON.parse(
+    fs.readFileSync(`${__dirname}/tours-complete.json`, 'utf-8')
+  );
 
-	const data = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, 'utf-8'))
+  try {
+    await Tour.create(data);
+    console.log('Data successfully imported.');
+  } catch (error) {
+    console.log('Error', error);
+  }
 
-	try {
-		await Tour.create(data)
-		console.log("Data successfully imported.")
-	} catch (error) {
-		console.log("Error", error)
-	}
-
-	process.exit();
-}
-
+  process.exit();
+};
 
 const deleteData = async (req, res) => {
+  try {
+    await Tour.deleteMany();
+    console.log('Data successfully delete.');
+  } catch (error) {
+    console.log('Error', error);
+  }
 
-	try {
-		await Tour.deleteMany()
-		console.log("Data successfully delete.")
-	} catch (error) {
-		console.log("Error", error)
-	}
+  process.exit();
+};
 
-	process.exit();
-}
-
-console.log(process.argv)
+console.log(process.argv);
 
 if (process.argv[2] === '-import') {
-	importData();
+  importData();
 }
 
 if (process.argv[2] === '-delete') {
-	deleteData();
+  deleteData();
 }
--- models/tourModel.js
+++ models/tourModel.js
@@ -2,105 +2,131 @@ const mongoose = require('mongoose');
 const slugify = require('slugify');
 //const validate = require('mongoose-validator');
 
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'Tour must have a name.'],
-    unique: true,
-    trim: true,
-    minlength: [5, `A tour name must have a min of 5 characters`],
-    maxlength: [40, 'A tour name must have a max of 40 characters'],
-    //validate: [validator.isAlpha, 'Nume must have only letters'] // does not allow spaces
-  },
-  duration: {
-    type: Number,
-    required: [true, 'Tour must have a duration.']
-  },
-  maxGroupSize: {
-    type: Number,
-    required: [true, 'Tour must have a group size.']
-  },
-  difficulty: {
-    type: String,
-    required: [true, 'A tour must have a difficulty'],
-    enum: {
-      values: ['easy', 'medium', 'difficult'],
-      message: 'Difficulty is either: easy, medium, difficult'
-    }
-  },
-  rating: {
-    type: Number,
-    default: 0,
-    min: [1, 'A tour must have a min ratting value of 1.0'],
-    max: [5, 'A tour must have a max ratting value of 5.0'],
-  },
-  ratingsAverage: {
-    type: Number,
-    default: 0
-  },
-  ratingsQuantity: {
-    type: Number,
-    default: 0
-  },
-  price: {
-    type: Number,
-    required: [true, 'Tour must have a price.']
-  },
-  priceDiscount: {
-    type: Number,
-    validate: {
-      validator: function (val) {
-        return val < this.price
+const tourSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Tour must have a name.'],
+      unique: true,
+      trim: true,
+      minlength: [5, `A tour name must have a min of 5 characters`],
+      maxlength: [40, 'A tour name must have a max of 40 characters'],
+      //validate: [validator.isAlpha, 'Nume must have only letters'] // does not allow spaces
+    },
+    duration: {
+      type: Number,
+      required: [true, 'Tour must have a duration.'],
+    },
+    maxGroupSize: {
+      type: Number,
+      required: [true, 'Tour must have a group size.'],
+    },
+    difficulty: {
+      type: String,
+      required: [true, 'A tour must have a difficulty'],
+      enum: {
+        values: ['easy', 'medium', 'difficult'],
+        message: 'Difficulty is either: easy, medium, difficult',
       },
-      message: 'Dicount price must be lower than the price.'
-    }
+    },
+    rating: {
+      type: Number,
+      default: 1,
+      min: [1, 'A tour must have a min rating value of 1.0'],
+      max: [5, 'A tour must have a max rating value of 5.0'],
+    },
+    ratingsAverage: {
+      type: Number,
+      default: 0,
+    },
+    ratingsQuantity: {
+      type: Number,
+      default: 0,
+    },
+    price: {
+      type: Number,
+      required: [true, 'Tour must have a price.'],
+    },
+    priceDiscount: {
+      type: Number,
+      validate: {
+        validator: function (val) {
+          return val < this.price;
+        },
+        message: 'Dicount price must be lower than the price.',
+      },
+    },
+    summary: {
+      type: String,
+      required: [true, 'A tour must have a summary'],
+      trim: true, // Removes blanc spaces
+    },
+    description: {
+      type: String,
+      trim: true,
+    },
+    imageCover: {
+      type: String,
+      required: [true, 'A tour must have cover image.'],
+    },
+    images: [String],
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+      select: false,
+    },
+    startDates: [Date],
+    vip: {
+      type: Boolean,
+      default: false,
+    },
+    startLocation: {
+      type: {
+        type: String,
+        enum: ['Point'],
+        default: 'Point',
+      },
+      coordinates: [Number],
+      address: String,
+      description: String,
+    },
+    locations: [
+      {
+        type: {
+          type: String,
+          enum: ['Point'],
+          default: 'Point',
+        },
+        coordinates: [Number],
+        address: String,
+        description: String,
+        day: Number,
+      },
+    ],
+    slug: String,
   },
-  summary: {
-    type: String,
-    required: [true, 'A tour must have a summary'],
-    trim: true // Removes blanc spaces
-  },
-  description: {
-    type: String,
-    trim: true
-  },
-  imageCover: {
-    type: String,
-    required: [true, 'A tour must have cover image.']
-  },
-  images: [String],
-  createdAt: {
-    type: Date,
-    default: Date.now(),
-    select: false
-  },
-  startDates: [Date],
-  vip: {
-    type: Boolean,
-    default: false
-  },
-  slug: String
-},
   {
     toJSON: { virtuals: true },
-    toObject: { virtuals: true }
+    toObject: { virtuals: true },
   }
 );
 
 // Document middleware
 tourSchema.pre('save', function (next) {
-  this.slug = slugify(this.name, { lower: true })
+  this.slug = slugify(this.name, { lower: true });
   next();
 });
 
 // Querry middleware
-tourSchema.pre(/^find/, function (next) { // filter out all VIP tours be default
-  this.find({ vip: { $ne: true } })
-  this.startTime = Date.now()
+tourSchema.pre(/^find/, function (next) {
+  // filter out all VIP tours be default
+  this.find({ vip: { $ne: true } });
+  this.startTime = Date.now();
   next();
 });
 
-tourSchema.post(/^find/, function (docs, next) { // filter out all VIP tours be default
+tourSchema.post(/^find/, function (docs, next) {
+  // filter out all VIP tours be default
   console.log(` Took ${this.startTime - Date.now()} ms`);
   //console.log(docs)
   next();
@@ -110,7 +136,7 @@ tourSchema.post(/^find/, function (docs, next) { // filter out all VIP tours be 
 tourSchema.pre('aggregate', function (next) {
   this.pipeline().unshift({ $match: { vip: { $ne: true } } });
   next();
-})
+});
 
 tourSchema.virtual('durationWeeks').get(function () {
   return this.duration / 7;
