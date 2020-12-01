15f6406e747049dce7a3be4eb81179a0b05947be
Ionela Cristea <oanacristea.1007@yahoo.com>
2020-12-01 18:34:37 +0100

    created temporary booking checkout

--- controllers/bookingControllers.js
+++ controllers/bookingControllers.js
@@ -1,3 +1,4 @@
+const Booking = require('../models/bookingModel');
 const Tour = require('../models/tourModel');
 const catchAsync = require('../utils/catchAsync');
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
@@ -9,10 +10,21 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ['card'],
     mode: 'payment',
-    success_url: `https://localhost:3000/`, //`${req.protocol}/${req.get('host')}/`,
-    cancel_url: `https://localhost:3000/`, //`${req.protocol}/${req.get('host')}/tours/`,
+
+    /**
+     * This is not a secure implementation and it's only used for testing purpose.
+     * On production mode (when have access to it)
+     * USE STRIPE WEB HOOKS instead, to create a booking session.
+     */
+    success_url: `${req.protocol}://${req.get('host')}/?tour=${
+      req.params.tourId
+    }&user=${req.user.id}&price=${tour.price}`,
+
+    cancel_url: `${req.protocol}://${req.get('host')}/tours/`,
+
     // https://stripe.com/docs/payments/accept-a-payment#prefilling-customer-data
     customer_email: req.user.email, // data available from protected route
+
     // TODO:
     client_reference_id: req.params.tourId,
     // product data
@@ -38,4 +50,14 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
     status: 'Success',
     session,
   });
+});
+
+exports.createBookingCheckout = catchAsync(async (req, res, next) => {
+  // TEMPORARY use as not secured
+  const { user, tour, price } = req.query;
+
+  if (!user && !tour && !price) return next(); // next is getOverview page
+
+  await Booking.create({ user, tour, price });
+  res.redirect(req.originalUrl.split('?')[0]);
 });
